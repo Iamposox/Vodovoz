fe9cf80c66a0d6dd26cf76d807f67853d03877c3
Andrey Gankov <gav@qsolution.ru>
2019-02-01 17:51:31 +0300

    Удалил закомментированный код, который больше не понадобился.

--- VodovozBusiness/Repositories/OrderRepository.cs
+++ VodovozBusiness/Repositories/OrderRepository.cs
@@ -265,17 +265,6 @@ namespace Vodovoz.Repository
 					  .List().FirstOrDefault();
 		}
 
-		//internal static Func<IUnitOfWork, string, Domain.Orders.Order> GetOrderByOnlineStoreIdTestGap;
-		//public static Domain.Orders.Order GetOrderByOnlineStoreId(IUnitOfWork uow, string onlineStoreId)
-		//{
-		//	if(GetOrderByOnlineStoreIdTestGap != null)
-		//		return GetOrderByOnlineStoreIdTestGap(uow, onlineStoreId);
-
-		//	return uow.Session.QueryOver<Domain.Orders.Order>()
-		//		.Where(x => x.OnlineStoreId == onlineStoreId)
-		//		.SingleOrDefault();
-		//}
-
 		public static OrderStatus[] GetOnClosingOrderStatuses()
 		{
 			return new OrderStatus[] {
