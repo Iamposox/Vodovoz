749d5f8a932a2af3dab827bc59e7d14c66bafe49
ShubArseniy <shub.ar@yandex.ru>
2021-03-01 15:34:11 +0300

    DriverCarKind

--- Vodovoz/Filters/ViewModels/CarJournalFilterViewModel.cs
+++ Vodovoz/Filters/ViewModels/CarJournalFilterViewModel.cs
@@ -1,7 +1,7 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using QS.Project.Filter;
 using QS.Project.Journal;
-using QS.Services;
+using Vodovoz.Domain.Logistic;
 
 namespace Vodovoz.Filters.ViewModels
 {
@@ -9,15 +9,25 @@ namespace Vodovoz.Filters.ViewModels
 	{
 		public CarJournalFilterViewModel()
 		{
-			UpdateWith(
-				x => x.IncludeArchive
-			);
+			includeVisitingMasters = true;
 		}
-
+		
 		private bool includeArchive;
 		public virtual bool IncludeArchive {
 			get => includeArchive;
-			set => SetField(ref includeArchive, value, () => IncludeArchive);
+			set => UpdateFilterField(ref includeArchive, value);
+		}
+		
+		private bool includeVisitingMasters;
+		public bool IncludeVisitingMasters {
+			get => includeVisitingMasters;
+			set => UpdateFilterField(ref includeVisitingMasters, value);
+		}
+		
+		private IList<CarTypeOfUse> restrictedCarTypesOfUse;
+		public IList<CarTypeOfUse> RestrictedCarTypesOfUse {
+			get => restrictedCarTypesOfUse;
+			set => UpdateFilterField(ref restrictedCarTypesOfUse, value);
 		}
 	}
 }
